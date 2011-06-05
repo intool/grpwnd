a805bb2d16833970ca0f52b45049168792dea306
Alan Touring <grpwnd@gmail.com>
2011-06-05 01:05:53 +0100

    Crawlie is working... :-)

--- betadash/crawlie/crawlie.js
+++ betadash/crawlie/crawlie.js
@@ -1,9 +1,25 @@
-require('/usr/local/lib/node_modules/node.io').scrape(function() {
-    this.getHtml('http://www.reddit.com/', function(err, $) {
-        var stories = [];
-        $('a.title').each(function(title) {
-            stories.push(title.text);
-        });
-        this.emit(stories);
-    });
+var io = require('/usr/local/lib/node_modules/node.io'),
+    sys = require('sys'),
+    fs = require('fs');
+
+var period = 10000;
+// var interval = setInterval(function() {
+
+io.scrape(function() {
+        var country = 'UK';
+    	this.get('http://api.groupon.de/api/v1/deals/oftheday/' +country +'/', function(err, text) {
+	var timestamp = Date.now();
+	var fd = '/home/user/grpwnd/betadash/crawlie/cache/' +country +'_' +timestamp +'.xml';
+	this.emit(text);
+
+	this.emit('File saved : ' +fd);
+		fs.writeFile(fd, text, function (err) {
+			if (err){ 
+				throw err;
+                		console.log('OOps! Failed to Write File!');
+			}
+		});	
+	});
+
 });
+// }, period);
